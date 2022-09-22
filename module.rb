31c4f5d7d99494e85811b83ff919ce7f796d6257
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-09-22 12:45:53 +0100

    create all enumerable

--- module.rb
+++ module.rb
@@ -3,13 +3,13 @@ module MyEnumerable
     @list ||= []
   end
 
-#   def all?
-#     oyin = []
-#     list.each do |said|
-#       oyin.push(said) if yield(said)
-#     end
-#     puts oyin.length == list.length
-#   end
+  def all?
+    oyin = []
+    list.each do |said|
+      oyin.push(said) if yield(said)
+    end
+    puts oyin.length == list.length
+  end
 
 #   def any?
 #     oyin = []
