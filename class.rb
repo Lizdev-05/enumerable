b9ccc06d57162c3ce8a4a905862952b85bfbed43
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-09-22 12:59:52 +0100

    add condition check for list items

--- class.rb
+++ class.rb
@@ -10,3 +10,7 @@ end
 list = Mylist.new(1, 2, 3, 4)
 list.all? { |e| e < 5 } # => true
 list.all? { |e| e > 5 } # => false
+
+# This is for any method
+list.any? { |e| e == 2 } # => true
+list.any? { |e| e == 5 } # => false
